4b3406f9f1cdedef83017389e547e9c5dbbb048d
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-05-13 13:50:00 +0700

    DB version is working, need to fix the rectangle

--- label_folder.py
+++ label_folder.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, session
-from models import Image, Label, SkippedImage
+from models import Image, Label, SkippedImage, Annotation
 import cv2
 import numpy as np
 import base64
@@ -10,63 +10,56 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    folders = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5']
-    return render_template('index.html', folders=folders)
+    return redirect(url_for('main.label_images'))
 
 
+@main.route('/label', methods=['GET', 'POST'])
+def label_images():
+    image = Image.query \
+        .select_from(Image) \
+        .outerjoin(Annotation, Image.id == Annotation.image_id) \
+        .outerjoin(Label, Annotation.id == Label.annotation_id) \
+        .outerjoin(SkippedImage, Image.id == SkippedImage.image_id) \
+        .filter(Label.id.is_(None)) \
+        .filter(SkippedImage.id.is_(None)).first()
 
-@main.route('/label/<folder>', methods=['GET', 'POST'])
-def label_folder(folder):
-    unlabeled_images = Image.query.filter_by(folder=folder).outerjoin(Label).filter(Label.id == None).outerjoin(SkippedImage).filter(SkippedImage.id == None).all()
-
-    if not unlabeled_images:
+    if not image:
         return render_template('unlabeled.html')
 
-    image = unlabeled_images[0]
-    image_data = cv2.imdecode(np.frombuffer(image.image_data, np.uint8), cv2.IMREAD_COLOR)
+    image_data = cv2.imdecode(np.frombuffer(image.image_data, np.uint8),
+                              cv2.IMREAD_COLOR)
     annotated_image = image_data.copy()
     original_height, original_width = annotated_image.shape[:2]
 
-    annotation_data = image.annotation_data
-    annotations = []
-    if annotation_data:
-        annotations = annotation_data.split('\n')
+    annotations = [ann.annotation_data for ann in image.annotations]
 
     upper_label_xy = None
     lower_label_xy = None
 
-    for annotation in annotations:
-        if not annotation:
-            continue
-        class_id, x_center, y_center, width, height = map(float, annotation.split())
-        x1 = int((x_center - width / 2) * original_width)
-        y1 = int((y_center - height / 2) * original_height)
-        x2 = int((x_center + width / 2) * original_width)
-        y2 = int((y_center + height / 2) * original_height)
+    for set_annotation in annotations:
+        for annotation in set_annotation.split("\n")[:-1]:
+            class_id, x_center, y_center, width, height = map(float, annotation.split())
+            x1 = int((x_center - width / 2) * original_width)
+            y1 = int((y_center - height / 2) * original_height)
+            x2 = int((x_center + width / 2) * original_width)
+            y2 = int((y_center + height / 2) * original_height)
 
-        if upper_label_xy is None or y1 < upper_label_xy[1]:
-            upper_label_xy = [x1, y1, x2, y2]
-        else:
-            lower_label_xy = [x1, y1, x2, y2]
+            if upper_label_xy is None or y1 < upper_label_xy[1]:
+                upper_label_xy = [x1, y1, x2, y2]
+            else:
+                lower_label_xy = [x1, y1, x2, y2]
 
     annotated_image = resize_image(annotated_image)
     resized_height, resized_width = annotated_image.shape[:2]
 
-    if upper_label_xy is not None:
-        x1, y1, x2, y2 = upper_label_xy
-        x1_resized = int(x1 * resized_width / original_width)
-        y1_resized = int(y1 * resized_height / original_height)
-        x2_resized = int(x2 * resized_width / original_width)
-        y2_resized = int(y2 * resized_height / original_height)
-        cv2.rectangle(annotated_image, (x1_resized, y1_resized), (x2_resized, y2_resized), (0, 255, 0), 2)
-
-    if lower_label_xy is not None:
-        x1, y1, x2, y2 = lower_label_xy
-        x1_resized = int(x1 * resized_width / original_width)
-        y1_resized = int(y1 * resized_height / original_height)
-        x2_resized = int(x2 * resized_width / original_width)
-        y2_resized = int(y2 * resized_height / original_height)
-        cv2.rectangle(annotated_image, (x1_resized, y1_resized), (x2_resized, y2_resized), (0, 255, 0), 2)
+    for label_xy in [upper_label_xy, lower_label_xy]:
+        if label_xy:
+            x1, y1, x2, y2 = label_xy
+            x1_resized = int(x1 * resized_width / original_width)
+            y1_resized = int(y1 * resized_height / original_height)
+            x2_resized = int(x2 * resized_width / original_width)
+            y2_resized = int(y2 * resized_height / original_height)
+            cv2.rectangle(annotated_image, (x1_resized, y1_resized), (x2_resized, y2_resized), (0, 255, 0), 2)
 
     if request.method == 'POST':
         if 'skip' in request.form:
@@ -74,44 +67,58 @@ def label_folder(folder):
             db.session.add(skipped_image)
             db.session.commit()
             session.clear()
-            return redirect(url_for('label_folder', folder=folder))
+            return redirect(url_for('main.label_images'))
 
-        choice = request.form['choice']
-        if 'upper_label' not in session:
-            session['upper_label'] = choice
-            question = 'Select the lower cloth:'
-            choices = ['Trousers', 'Shorts', 'Skirt']
-        elif 'lower_label' not in session:
-            session['lower_label'] = choice
-            question = 'Select the shoes:'
-            choices = ['Sandals', 'Sneakers', 'High heels', 'Leather shoes', 'None']
-        else:
-            session['shoe_label'] = choice
-            label = Label(
-                image_id=image.id,
-                upper_label=session['upper_label'],
-                lower_label=session['lower_label'],
-                shoe_label=session['shoe_label'],
-                upper_label_x=upper_label_xy[0] if upper_label_xy else None,
-                upper_label_y=upper_label_xy[1] if upper_label_xy else None,
-                upper_label_width=upper_label_xy[2] - upper_label_xy[0] if upper_label_xy else None,
-                upper_label_height=upper_label_xy[3] - upper_label_xy[1] if upper_label_xy else None,
-                lower_label_x=lower_label_xy[0] if lower_label_xy else None,
-                lower_label_y=lower_label_xy[1] if lower_label_xy else None,
-                lower_label_width=lower_label_xy[2] - lower_label_xy[0] if lower_label_xy else None,
-                lower_label_height=lower_label_xy[3] - lower_label_xy[1] if lower_label_xy else None
-            )
-            db.session.add(label)
-            db.session.commit()
-            session.clear()
-            return redirect(url_for('label_folder', folder=folder))
+        print("Process label form:", request.form['choice'])
+        process_label_form(request, image, upper_label_xy, lower_label_xy)
+
+    question, choices = get_label_questions()
+    encoded_image = base64.b64encode(cv2.imencode('.jpg', annotated_image)[1]).decode('utf-8')
+    return render_template('label.html', image=encoded_image, question=question, choices=choices)
+
+
+def get_label_questions():
+    if 'upper_label' not in session:
+        return 'Select the upper cloth:', ['Polo', 'Long sleeve T-shirt', 'Long sleeve shirt', 'Short sleeve Shirt', 'Short sleeve T-shirt',
+                                           'Sleeveless', 'Long sleeve blouse', 'Short sleeve blouse', 'Dress', 'Outwear']
+    elif 'lower_label' not in session:
+        return 'Select the lower cloth:', ['Trousers', 'Shorts', 'Skirt']
     else:
-        session.clear()
-        question = 'Select the upper cloth:'
-        choices = ['Polo', 'Long sleeve T-shirt', 'Long sleeve shirt', 'Short sleeve Shirt', 'Short sleeve T-shirt',
-                   'Sleeveless', 'Long sleeve blouse', 'Short sleeve blouse', 'Dress', 'Outwear']
+        return 'Select the shoes:', ['Sandals', 'Sneakers', 'High heels', 'Leather shoes', 'None']
 
-    return render_template('label.html', image=base64.b64encode(cv2.imencode('.jpg', annotated_image)[1]).decode('utf-8'), question=question, choices=choices)
+
+def process_label_form(request, image, upper_label_xy, lower_label_xy):
+    choice = request.form['choice']
+    if 'upper_label' not in session:
+        session['upper_label'] = choice
+        print("Assign upper session with:", choice)
+    elif 'lower_label' not in session:
+        print("In Lower Label")
+        session['lower_label'] = choice
+    else:
+        session['shoe_label'] = choice
+        save_labels_to_db(image, session, upper_label_xy, lower_label_xy)
+        session.clear()
+        return redirect(url_for('main.label_images'))
+
+
+def save_labels_to_db(image, session_info, upper_label_xy, lower_label_xy):
+    label = Label(
+        image_id=image.id,
+        upper_label=session_info['upper_label'],
+        lower_label=session_info['lower_label'],
+        shoe_label=session_info['shoe_label'],
+        upper_label_x=upper_label_xy[0] if upper_label_xy else None,
+        upper_label_y=upper_label_xy[1] if upper_label_xy else None,
+        upper_label_width=upper_label_xy[2] - upper_label_xy[0] if upper_label_xy else None,
+        upper_label_height=upper_label_xy[3] - upper_label_xy[1] if upper_label_xy else None,
+        lower_label_x=lower_label_xy[0] if lower_label_xy else None,
+        lower_label_y=lower_label_xy[1] if lower_label_xy else None,
+        lower_label_width=lower_label_xy[2] - lower_label_xy[0] if lower_label_xy else None,
+        lower_label_height=lower_label_xy[3] - lower_label_xy[1] if lower_label_xy else None
+    )
+    db.session.add(label)
+    db.session.commit()
 
 
 def resize_image(image, max_width=800, max_height=400):

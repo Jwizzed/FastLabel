a965df24b5dfd9568ab42863215cd72253a3a0e8
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-05-14 10:03:41 +0700

    Fixed image bug

--- label_folder.py
+++ label_folder.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, session
 from models import Image, Label, SkippedImage, Annotation
+from sqlalchemy.sql.expression import func
 import cv2
 import numpy as np
 import base64
@@ -15,14 +16,16 @@ def index():
 
 @main.route('/label', methods=['GET', 'POST'])
 def label_images():
-    image = Image.query \
-        .outerjoin(SkippedImage, Image.id == SkippedImage.image_id) \
-        .filter(SkippedImage.id.is_(None)) \
-        .outerjoin(Label, Image.id == Label.image_id) \
-        .filter(Label.id.is_(None)) \
-        .first()
+    if 'image_id' in session:
+        image_id = session['image_id']
+        image = Image.query.get(image_id)
+    else:
+        image = Image.query.order_by(func.random()).first()
+        if image:
+            session['image_id'] = image.id
 
     if not image:
+        session.pop('image_id', None)
         return render_template('unlabeled.html')
 
     image_data = cv2.imdecode(np.frombuffer(image.image_data, np.uint8),
@@ -60,19 +63,54 @@ def label_images():
             cv2.rectangle(annotated_image, (x1_resized, y1_resized), (x2_resized, y2_resized), (0, 255, 0), 2)
 
     if request.method == 'POST':
-        if 'skip' in request.form:
-            skipped_image = SkippedImage(image_id=image.id)
-            db.session.add(skipped_image)
-            db.session.commit()
-            session.clear()
-            return redirect(url_for('main.label_images'))
+        response = process_label_form(request, image, upper_label_xy, lower_label_xy)
+        if response:
+            return response
 
-        print("Process label form:", request.form['choice'])
-        process_label_form(request, image, upper_label_xy, lower_label_xy)
+    return render_current_image(image)
+
+
+def render_current_image(image):
+    image_data = cv2.imdecode(np.frombuffer(image.image_data, np.uint8), cv2.IMREAD_COLOR)
+    annotated_image = image_data.copy()
+    original_height, original_width = annotated_image.shape[:2]
+
+    annotations = Annotation.query.filter_by(image_id=image.id).all()
+    upper_label_xy = None
+    lower_label_xy = None
+
+    for set_annotation in annotations:
+        for annotation in set_annotation.annotation_data.split("\n"):
+            if annotation.strip():  # Ensure the annotation line is not empty
+                class_id, x_center, y_center, width, height = map(float, annotation.split())
+                x1 = int((x_center - width / 2) * original_width)
+                y1 = int((y_center - height / 2) * original_height)
+                x2 = int((x_center + width / 2) * original_width)
+                y2 = int((y_center + height / 2) * original_height)
+
+                if upper_label_xy is None or y1 < upper_label_xy[1]:
+                    upper_label_xy = [x1, y1, x2, y2]
+                else:
+                    lower_label_xy = [x1, y1, x2, y2]
+
+    annotated_image = resize_image(annotated_image, 800, 400)
+    draw_annotations(annotated_image, upper_label_xy, lower_label_xy, original_width, original_height)
 
     question, choices = get_label_questions()
     encoded_image = base64.b64encode(cv2.imencode('.jpg', annotated_image)[1]).decode('utf-8')
     return render_template('label.html', image=encoded_image, question=question, choices=choices)
+
+
+def draw_annotations(image, upper_label_xy, lower_label_xy, original_width, original_height):
+    resized_height, resized_width = image.shape[:2]
+    for label_xy in [upper_label_xy, lower_label_xy]:
+        if label_xy:
+            x1, y1, x2, y2 = label_xy
+            x1_resized = int(x1 * resized_width / original_width)
+            y1_resized = int(y1 * resized_height / original_height)
+            x2_resized = int(x2 * resized_width / original_width)
+            y2_resized = int(y2 * resized_height / original_height)
+            cv2.rectangle(image, (x1_resized, y1_resized), (x2_resized, y2_resized), (0, 255, 0), 2)
 
 
 def get_label_questions():
@@ -87,13 +125,21 @@ def get_label_questions():
 
 def process_label_form(request, image, upper_label_xy, lower_label_xy):
     choice = request.form['choice']
+    if 'skip' in request.form:
+        skipped_image = SkippedImage(image_id=image.id)
+        db.session.add(skipped_image)
+        db.session.commit()
+        session.clear()
+        return redirect(url_for('main.label_images'))
+
     if 'upper_label' not in session:
+        print("In upper, choice:", choice)
         session['upper_label'] = choice
-        print("Assign upper session with:", choice)
     elif 'lower_label' not in session:
-        print("In Lower Label")
+        print("In lower, choice:", choice)
         session['lower_label'] = choice
     else:
+        print("In shoe, choice:", choice)
         session['shoe_label'] = choice
         save_labels_to_db(image, session, upper_label_xy, lower_label_xy)
         session.clear()
@@ -115,7 +161,7 @@ def save_labels_to_db(image, session_info, upper_label_xy, lower_label_xy):
         lower_label_width=lower_label_xy[2] - lower_label_xy[0] if lower_label_xy else None,
         lower_label_height=lower_label_xy[3] - lower_label_xy[1] if lower_label_xy else None
     )
-    print("Saved label")
+    print("Saved label, upper:", session_info["upper_label"], "lower:", session_info["lower_label"])
     db.session.add(label)
     db.session.commit()
 

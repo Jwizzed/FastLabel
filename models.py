3ecce4ae6232906a033aa4e96acbc7d2f14aea5b
noeyiue <parat.kantida@gmail.com>
2024-09-15 13:25:20 +0700

    schema: Define new database structure

--- models.py
+++ models.py
@@ -1,5 +1,28 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+class RegisterKey(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key_hash = db.Column(db.String(256), nullable=False)
+
+    def set_register_key(self, key):
+        self.key_hash = generate_password_hash(key)
+
+    def check_register_key(self, key):
+        return check_password_hash(self.key_hash, key)
 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,3 +61,45 @@ class InvalidImage(db.Model) :
     group_id = db.Column(db.Integer)
     image_name = db.Column(db.String(100))
     image_data = db.Column(db.LargeBinary)
+
+
+class PredictedResult(db.Model) :
+    id = db.Column(db.Integer, primary_key=True)
+    image_name = db.Column(db.String(100))
+    ethnicity = db.Column(db.String(100))
+    age = db.Column(db.String(100))
+    gender = db.Column(db.String(100))
+    hair_length = db.Column(db.String(100))
+    upper_body_length = db.Column(db.String(100))
+    upper_body_color = db.Column(db.String(100))
+    upper_body_type = db.Column(db.String(100))
+    lower_body_length = db.Column(db.String(100))
+    lower_body_color = db.Column(db.String(100))
+    lower_body_type = db.Column(db.String(100))
+    footwear = db.Column(db.String(100))
+    backpack = db.Column(db.String(100))
+    bag = db.Column(db.String(100))
+    glasses = db.Column(db.String(100))
+    hat = db.Column(db.String(100))
+    mask = db.Column(db.String(100))
+
+
+class GtData(db.Model) :
+    id = db.Column(db.Integer, primary_key=True)
+    image_name = db.Column(db.String(100))
+    ethnicity = db.Column(db.String(100))
+    age = db.Column(db.String(100))
+    gender = db.Column(db.String(100))
+    hair_length = db.Column(db.String(100))
+    upper_body_length = db.Column(db.String(100))
+    upper_body_color = db.Column(db.String(100))
+    upper_body_type = db.Column(db.String(100))
+    lower_body_length = db.Column(db.String(100))
+    lower_body_color = db.Column(db.String(100))
+    lower_body_type = db.Column(db.String(100))
+    footwear = db.Column(db.String(100))
+    backpack = db.Column(db.String(100))
+    bag = db.Column(db.String(100))
+    glasses = db.Column(db.String(100))
+    hat = db.Column(db.String(100))
+    mask = db.Column(db.String(100))
